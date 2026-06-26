da3885bccd6abcfd26d18f2f8ac234496529b19b
Giorgio Peluso <giorgiodabest1@gmail.com>
2026-06-26 23:52:05 +0100

    Expose release CLI diagnostics and state commands

--- native/include/awfan/native_cli.hpp
+++ native/include/awfan/native_cli.hpp
@@ -7,7 +7,10 @@ int run_native_temps(bool json_output);
 int run_native_fans(bool json_output);
 int run_native_profiles(bool json_output);
 int run_native_watch(int seconds, bool temperatures_only);
-int run_native_set_boost(int cpu_percent, int gpu_percent, bool confirmed, bool json_output);
+int run_native_set_boost(int cpu_value, int gpu_value, bool confirmed, bool json_output);
 int run_native_set_profile(int profile_index, bool confirmed, bool json_output);
+int run_native_doctor(bool json_output);
+int run_native_state(bool json_output);
+int run_native_clear_state();
 
 }  // namespace awfan
