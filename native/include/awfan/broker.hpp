dc22f1a4a7c4721696fbdfc83c5de467944f11d2
Giorgio <giorgiodabest1@gmail.com>
2026-06-27 22:43:06 +0100

    Expose captured broker responses

--- native/include/awfan/broker.hpp
+++ native/include/awfan/broker.hpp
@@ -5,9 +5,15 @@
 
 namespace awfan {
 
+struct BrokerResponse {
+    int exit_code{1};
+    std::string output;
+};
+
 bool is_process_elevated();
 bool command_requires_broker(const std::wstring& command);
 bool broker_is_available();
+std::optional<BrokerResponse> request_broker(int argc, wchar_t** argv);
 std::optional<int> forward_to_broker(int argc, wchar_t** argv);
 int run_broker_server();
 int run_broker_self_test();
